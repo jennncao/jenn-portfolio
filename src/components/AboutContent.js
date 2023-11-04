5c458a5104f43c4a300dd647bfb42af82bf5b539
jennncao <95658371+jennncao@users.noreply.github.com>
2023-11-04 15:49:20 -0400

    Update AboutContent.js

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -11,11 +11,11 @@ const AboutContent = () => {
         </h1>
         <p>
             Hello, nice to meet you! My name is Jennifer Cao and I am currently a senior at the University of Pittsburgh studying Computer 
-            Science + Digital Narrative and Interactive Design (DNID). I mostly focused on back-end developnebt, though my affinty towards
+            Science + Digital Narrative and Interactive Design (DNID). I have mostly focused on back-end development, though my affinty towards
             design does draw me towards front-end development as well. As someone who does particularly love narrative-based video games,
-            I have also dabbled in game design, particularly on the storytelling side. I am originally from New Jersey in the New 
-            York Metropolitan Area. Outside of computer science related areas, I enjoy art, music, travelling, and good food (a live-to-
-            eat type of person, if you will). {
+            I have also dabbled in game design, particularly on the storytelling side though I am currently working on a game as a capstone project.
+            I am originally from New Jersey in the New York Metropolitan Area. Outside of computer science related areas, I enjoy art, music, 
+            travelling, and good food (a live-to-eat type of person, if you will). {
                 
             }
         </p>
